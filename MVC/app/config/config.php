4bd48662dfa079cbd1df664d3d8998eec2097f08
Reimar Rosas <reimarrosas0208@gmail.com>
2022-03-10 08:52:14 -0500

    Initial files

---
+++ MVC/app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'YOUR_URLROOT');
+  // Site Name
+  define('SITENAME', 'YOUR_SITENAME');
+
+  // Database Params
+  define('DB_HOST', 'YOUR_DB_HOST');
+  define('DB_USER', 'YOUR_DB_USER');
+  define('DB_PASS', 'YOUR_DB_PASSWORD');
+  define('DB_NAME', 'YOUR_DB_NAME');
---
+++ MVC/app/views/Home/home.php
@@ -0,0 +1,18 @@
+<?php require APPROOT . '/views/includes/header.php'; ?>
+    <h1 class="container">Home View</h1>
+    <div class="jumbotron container">
+        <h2> This is the entry point of the MVC framework. </h2> <br>
+        To use this framework:
+        <ul>
+            <li>Start with updating the configuration in <pre style="display:inline;">/app/config/config.php </pre>   </li>
+        </ul>
+        According to the features,
+        <ul>
+            <li>Write the controller classes in the <pre style="display:inline;">/app/controllers </pre>folder</li>
+            <li>Write the model classes in the <pre style="display:inline;">/app/models </pre>folder</li>
+            <li>Write the views in the <pre style="display:inline;">/app/views </pre>folder</li>
+        </ul>
+            
+        
+</div>
+<?php require APPROOT . '/views/includes/footer.php'; ?>
