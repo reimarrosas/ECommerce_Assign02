3d92fc9c8f43cf9b488f4dc11c3c2ba5efd2c20b
Reimar Rosas <reimarrosas0208@gmail.com>
2022-03-14 18:24:52 -0400

    Added a controller method that checks if the user is logged in or has profile in the session

--- Blog/app/core/Controller.php
+++ Blog/app/core/Controller.php
@@ -1,26 +1,36 @@
 <?php
-    class Controller{
-        /*
+class Controller
+{
+    /*
             This is our base controller 
             It will have two functions - one to invoke views and one to invoke models 
         */
 
-        public function view($name, $data = []){
-            if(file_exists('../app/views/'.$name.'.php')){
-                require_once('../app/views/'.$name.'.php');
-            }
-            else{
-                echo '../app/views/'.$name.'.php does not exists';
-            }
+    public function view($name, $data = [])
+    {
+        if (file_exists('../app/views/' . $name . '.php')) {
+            require_once('../app/views/' . $name . '.php');
+        } else {
+            echo '../app/views/' . $name . '.php does not exists';
         }
-
-        public function model($modelName){
-            require_once '../app/models/'.$modelName.'.php';
-            return new $modelName;
-        }
-
-
-
-
     }
-?>
+
+    public function model($modelName)
+    {
+        require_once '../app/models/' . $modelName . '.php';
+        return new $modelName;
+    }
+
+    public function isAuthorized($profileId)
+    {
+        if (!isLoggedIn()) {
+            echo '<meta http-equiv="refresh" content="0;url=' . URLROOT . '/login" />';
+        } else if (!$profileId) {
+            echo '<meta http-equiv="refresh" content="0;url=' . URLROOT . '/profile" />';
+        } else {
+            return true;
+        }
+
+        return false;
+    }
+}
