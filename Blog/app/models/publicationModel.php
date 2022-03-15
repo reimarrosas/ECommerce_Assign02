563a449d79dbf28632521a2013773271c8e3aaa4
Reimar Rosas <reimarrosas0208@gmail.com>
2022-03-14 20:03:30 -0400

    Implemented marking the publication as private or public

---
+++ Blog/app/models/publicationModel.php
@@ -0,0 +1,26 @@
+<?php
+
+class publicationModel extends Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function markAsOpposite($publicationId, $profileId)
+    {
+        $this->query('SELECT * FROM publications WHERE publication_id = :publicationId AND profile_id = :profileId');
+        $this->bind('publicationId', $publicationId);
+        $this->bind('profileId', $profileId);
+        $publication = $this->getSingle();
+        if (isset($publication)) {
+            $this->query('UPDATE publications SET publication_status = :publicationStatus WHERE publication_id = :publicationId AND profile_id = :profileId');
+            $this->bind('publicationStatus', $publication->publication_status == 'private' ? 'public' : 'private');
+            $this->bind('publicationId', $publicationId);
+            $this->bind('profileId', $profileId);
+            return $this->execute();
+        }
+
+        return false;
+    }
+}
