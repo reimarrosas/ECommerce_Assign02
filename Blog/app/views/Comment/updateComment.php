39fd2149e8c60b224307ef235fd107af4d958244
Reimar Rosas <reimarrosas0208@gmail.com>
2022-03-18 21:17:48 -0400

    Addeda POST method on the update comment form and removed the whitespace inside the textarea

--- Blog/app/views/Comment/updateComment.php
+++ Blog/app/views/Comment/updateComment.php
@@ -2,16 +2,14 @@
 <div class="wrapper">
     <div class="form-group">
         <div class="update-form">
-            <form action="">
+            <form action="" method="POST">
                 <h1>Update Comment</h1>
                 <?php if (isset($data['error'])) : ?>
                     <div class="alert alert-danger">
                         <?= $data['error'] ?>
                     </div>
                 <?php endif; ?>
-                <textarea class="update-form-comment" id="pub_comment" maxlength="200" placeholder="Comment here...[Limit:200 Characters]" name="pub_comment" cols="30" rows="10">
-                    <?= $data['pub_comment'] ?>
-                </textarea>
+                <textarea class="update-form-comment" id="pub_comment" maxlength="200" placeholder="Comment here...[Limit:200 Characters]" name="pub_comment" cols="30" rows="10"><?= $data['pub_comment']->comment ?></textarea>
                 <button class="button-primary" name="confirm">Confirm</button>
             </form>
         </div>
