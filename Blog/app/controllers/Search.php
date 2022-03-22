fe6e4dd7284633f91851da443af3aa7a4a04dc0c
Astour Krikor <72988512+KrikorAstour@users.noreply.github.com>
2022-03-22 05:42:20 -0700

    Update Search.php

--- Blog/app/controllers/Search.php
+++ Blog/app/controllers/Search.php
@@ -3,10 +3,43 @@ class Search extends Controller
 {
     public function __construct()
     {
+        $this->searchModel = $this->model('searchModel');
     }
 
     public function index()
     {
+        $this->view('Search/search');
+    }
+    
+    public function getResultByTitle(){
+        
+        if(isset($_POST['search']) && $_POST['search_type'] == "Title"){
+            // var_dump($_POST['search_type']);
+            $data = $_POST['search_text'];
+            $isSucc = $this->searchModel->getResultByTitle($data);
+            var_dump($isSucc);
+            $this->view('Search/search', $isSucc);
+        }
+
+        else if(isset($_POST['search']) && $_POST['search_type'] == "Content"){
+            $data = $_POST['search_text'];
+            $isSucc = $this->searchModel->getResultByContent($data);
+            $this->view('Search/seaerch', $isSucc);
+        }
+
+        elseif ($_POST['search_type'] == "Latest"){
+            $isSucc = $this->searchModel->getResultByLatest();
+            $this->view('Search/search', $isSucc);
+        }
+    }
+
+    public function getResultByLatest(){
+        var_dump("latest");
+        $this->view('Search/search');
+    }
+
+    public function getResultByContent(){
+        var_dump("content");
         $this->view('Search/search');
     }
 }
